6d77b164500f4c52ac540e04919eb821fc6df591
quabug <quabug@gmail.com>
2022-01-23 18:57:58 +0800

    inject func/action and call (#2)
    * inject func/action and call
    
    * update version

--- Packages/com.quabug.one-shot-injection/OneShot.cs
+++ Packages/com.quabug.one-shot-injection/OneShot.cs
@@ -145,6 +145,19 @@ namespace OneShot
         {
             InjectAll(container, instance, typeof(T));
         }
+
+        public static TReturn InjectCall<TFunc, TReturn>([NotNull] this Container container, TFunc func) where TFunc : Delegate
+        {
+            var invoke = func.GetType().GetMethod("Invoke");
+            if (!typeof(TReturn).IsAssignableFrom(invoke.ReturnType)) throw new ArgumentException();
+            return (TReturn) invoke.Invoke(func, container.ResolveParameterInfos(invoke.GetParameters()));
+        }
+
+        public static void InjectCall<TAction>([NotNull] this Container container, TAction action) where TAction : Delegate
+        {
+            var invoke = action.GetType().GetMethod("Invoke");
+            invoke.Invoke(action, container.ResolveParameterInfos(invoke.GetParameters()));
+        }
     }
 
     class TypeInjector
@@ -172,6 +185,8 @@ namespace OneShot
                     case MethodInfo method: // TODO: check method validation
                         _methods.Add(method);
                         break;
+                    default:
+                        throw new NotSupportedException();
                 }
             }
         }
