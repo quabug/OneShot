d28ed6e48292853309b7209fee488eb113eaa0f4
quabug <quabug@gmail.com>
2022-08-25 14:42:23 +0800

    Label (#23)
    * type label
    
    * update version and projects

--- Packages/com.quabug.one-shot-injection/OneShot.cs
+++ Packages/com.quabug.one-shot-injection/OneShot.cs
@@ -36,7 +36,13 @@ namespace OneShot
     }
 
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property)]
-    public sealed class InjectAttribute : Attribute {}
+    public sealed class InjectAttribute : Attribute
+    {
+        public Type Label { get; }
+        public InjectAttribute(Type label = null) => Label = label;
+    }
+    
+    public interface ILabel<T> {}
 
     public class ResolverBuilder
     {
@@ -51,36 +57,37 @@ namespace OneShot
             _ConcreteType = concreteType;
         }
 
-        [NotNull] public ResolverBuilder As([NotNull] Type contractType)
+        [NotNull] public ResolverBuilder As([NotNull] Type contractType, Type label = null)
         {
-            if (!contractType.IsAssignableFrom(_ConcreteType)) throw new ArgumentException();
+            if (!contractType.IsAssignableFrom(_ConcreteType)) throw new ArgumentException($"concreteType({_ConcreteType}) must derived from contractType({contractType})", nameof(contractType));
+            if (label != null) contractType = label.CreateLabelType(contractType);
             var resolver = GetOrCreateResolver(contractType);
             if (!resolver.Contains(_Creator)) resolver.Insert(0, _Creator);
             return this;
         }
 
-        [NotNull] public ResolverBuilder As<T>()
+        [NotNull] public ResolverBuilder As<T>(Type label = null)
         {
-            return As(typeof(T));
+            return As(typeof(T), label);
         }
 
-        [NotNull] public ResolverBuilder AsSelf()
+        [NotNull] public ResolverBuilder AsSelf(Type label = null)
         {
-            return As(_ConcreteType);
+            return As(_ConcreteType, label);
         }
 
-        [NotNull] public ResolverBuilder AsInterfaces()
+        [NotNull] public ResolverBuilder AsInterfaces(Type label = null)
         {
-            foreach (var @interface in _ConcreteType.GetInterfaces()) As(@interface);
+            foreach (var @interface in _ConcreteType.GetInterfaces()) As(@interface, label);
             return this;
         }
 
-        [NotNull] public ResolverBuilder AsBases()
+        [NotNull] public ResolverBuilder AsBases(Type label = null)
         {
             var baseType = _ConcreteType.BaseType;
-            while (baseType != null)
+            while (baseType != null && baseType != typeof(Object) && baseType != typeof(ValueType))
             {
-                As(baseType);
+                As(baseType, label);
                 baseType = baseType.BaseType;
             }
             return this;
@@ -128,9 +135,19 @@ namespace OneShot
         
         public LifetimeBuilder With(params object[] instances)
         {
-            if (instances == null || !instances.Any()) return this;
+            return WithImpl(instances.Select(instance => (instance, (Type)null)));
+        }
+        
+        public LifetimeBuilder With(params (object instance, Type label)[] labeledInstances)
+        {
+            return WithImpl(labeledInstances);
+        }
+
+        private LifetimeBuilder WithImpl(IEnumerable<(object instance, Type label)> labeledInstances)
+        {
+            if (labeledInstances == null || !labeledInstances.Any()) return this;
             var container = _Container.CreateChildContainer();
-            foreach (var instance in instances) container.RegisterInstance(instance).AsSelf().AsBases().AsInterfaces();
+            foreach (var (instance, label) in labeledInstances) container.RegisterInstance(instance).AsSelf(label).AsBases(label).AsInterfaces(label);
             return new LifetimeBuilder(_Container, (_, contractType) => _Creator(container, contractType), _ConcreteType);
         }
     }
@@ -144,7 +161,6 @@ namespace OneShot
         private static readonly Dictionary<Container, Dictionary<Type, List<Func<Container, Type, object>>>> _containerResolvers =
             new Dictionary<Container, Dictionary<Type, List<Func<Container, Type, object>>>>()
         ;
-        
 
         [NotNull] public static Container CreateChildContainer([NotNull] this Container container)
         {
@@ -163,18 +179,19 @@ namespace OneShot
             return _containerResolvers.GetOrCreate(container).GetOrCreate(type);
         }
 
-        [NotNull] public static object Resolve([NotNull] this Container container, [NotNull] Type type)
+        [NotNull] public static object Resolve([NotNull] this Container container, [NotNull] Type type, Type label = null)
         {
             using (CircularCheck.Create())
             {
-                var creator = container.FindFirstCreatorsInHierarchy(type);
+                var creatorKey = label == null ? type : label.CreateLabelType(type);
+                var creator = container.FindFirstCreatorsInHierarchy(creatorKey);
                 return creator != null ? creator(container, type) : throw new ArgumentException($"{type.Name} have not been registered yet");
             }
         }
 
-        [NotNull] public static T Resolve<T>([NotNull] this Container container)
+        [NotNull] public static T Resolve<T>([NotNull] this Container container, Type label = null)
         {
-            return (T) container.Resolve(typeof(T));
+            return (T) container.Resolve(typeof(T), label);
         }
 
         [NotNull] private static IEnumerable<object> ResolveGroupWithoutException([NotNull] this Container container, Type type)
@@ -243,7 +260,7 @@ namespace OneShot
             using (CircularCheck.Create())
             {
                 var method = func.Method;
-                if (method.ReturnType == typeof(void)) throw new ArgumentException();
+                if (method.ReturnType == typeof(void)) throw new ArgumentException($"{method.Name} must return void", nameof(func));
                 return method.Invoke(func.Target, container.ResolveParameterInfos(method.GetParameters()));
             }
         }
@@ -335,8 +352,10 @@ namespace OneShot
 
         internal static object ResolveParameterInfo(this Container container, ParameterInfo parameter)
         {
+            var label = parameter.GetCustomAttribute<InjectAttribute>()?.Label;
             var parameterType = parameter.ParameterType;
-            var creator = container.FindFirstCreatorsInHierarchy(parameterType);
+            var creatorKey = label == null ? parameterType : label.CreateLabelType(parameterType);
+            var creator = container.FindFirstCreatorsInHierarchy(creatorKey);
             if (creator != null) return creator(container, parameterType);
 
             if (parameterType.IsArray)
@@ -437,7 +456,7 @@ namespace OneShot
 
         private void CheckInstanceType(object instance)
         {
-            if (instance.GetType() != _type) throw new ArgumentException();
+            if (instance.GetType() != _type) throw new ArgumentException($"mismatch types between {nameof(instance)}({instance.GetType()}) and type({_type})");
         }
     }
 
@@ -495,6 +514,22 @@ namespace OneShot
         public void Dispose()
         {
             _circularCheckSet.Clear();
+        }
+    }
+
+    static class LabelExtension
+    {
+        [NotNull]
+        public static Type CreateLabelType([NotNull] this Type label, [NotNull] Type contractType)
+        {
+            if (label.BaseType != null) throw new ArgumentException($"label {label.FullName} cannot have base type", nameof(label));
+            var interfaces = label.GetInterfaces();
+            if (interfaces.Length != 1 && interfaces[0].GetGenericTypeDefinition() != typeof(ILabel<>))
+                throw new ArgumentException($"label {label.FullName} must implement and only implement {nameof(ILabel<int>)}<> interface.", nameof(label));
+            var labelValueType = interfaces[0].GenericTypeArguments[0];
+            if (labelValueType.IsGenericParameter) label = label.MakeGenericType(contractType);
+            else if (labelValueType != contractType) throw new ArgumentException($"Type mismatch between typed label {label.FullName} and {contractType.FullName}");
+            return label;
         }
     }
 }
