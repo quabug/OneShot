194f5558f55f127950659efa3159fcce4161b08e
quabug <quabug@gmail.com>
2022-09-28 02:26:30 +0800

    more tests of generic

--- Assets/Tests/TestGeneric.cs
+++ Assets/Tests/TestGeneric.cs
@@ -21,5 +21,26 @@ namespace OneShot.Test
             container.Register(typeof(Generic<,>), (_, t) => Activator.CreateInstance(typeof(Generic<,>).MakeGenericType(t.GetGenericArguments()))).As(typeof(Generic<,>));
             Assert.That(container.Resolve(type), Is.TypeOf(type));
         }
+        
+        interface Label<T> : ILabel<T> {}
+        
+        [Test, TestCaseSource(nameof(_genericArguments))]
+        public void should_make_instance_of_generic_with_label(Type type)
+        {
+            var container = new Container();
+            container.Register(typeof(Generic<>), (_, t) => Activator.CreateInstance(typeof(Generic<>).MakeGenericType(t.GetGenericArguments()))).As(typeof(Generic<>), typeof(Label<>));
+            container.Register(typeof(Generic<,>), (_, t) => Activator.CreateInstance(typeof(Generic<,>).MakeGenericType(t.GetGenericArguments()))).As(typeof(Generic<,>), typeof(Label<>));
+            Assert.Catch<ArgumentException>(() => container.Resolve(type));
+            Assert.That(container.Resolve(type, typeof(Label<>)), Is.TypeOf(type));
+        }
+        
+        [Test]
+        public void should_make_instance_of_concrete_generic()
+        {
+            var container = new Container();
+            var instance = new Generic<int>();
+            container.RegisterInstance(instance).As<Generic<int>>();
+            Assert.That(container.Resolve<Generic<int>>(), Is.SameAs(instance));
+        }
     }
 }
