d1c2e3927b7490d0ef2264e4a93e477b85e60bd6
quabug <quabug@gmail.com>
2023-08-18 15:39:45 +0800

    add `RegisterGeneric` method (#36)
    * add `RegisterGeneric` method
    
    * Update dotnet.yml
    
    * Update nuget-publish.yml
    
    * Update release-upm.yml
    
    * Update unity-test.yml
    
    * fix test
    
    * Update dotnet.yml
    
    * Update dotnet.yml

--- Assets/Tests/TestGeneric.cs
+++ Assets/Tests/TestGeneric.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace OneShot.Test
@@ -41,6 +42,63 @@ namespace OneShot.Test
             var instance = new Generic<int>();
             container.RegisterInstance(instance).As<Generic<int>>();
             Assert.That(container.Resolve<Generic<int>>(), Is.SameAs(instance));
+        }
+
+        private static Lazy<T> CreateLazy<T>(Container container, Type _)
+        {
+            return new Lazy<T>(() => container.Resolve<T>());
+        }
+
+        [Test]
+        public void should_register_and_resolve_generic_type_by_method_name()
+        {
+            var container = new Container();
+            var creator = GetType().GetMethod(nameof(CreateLazy), BindingFlags.Static | BindingFlags.NonPublic);
+            container.RegisterGeneric(typeof(Lazy<>), creator).With(123).AsSelf();
+            Assert.That(container.Resolve<Lazy<int>>().Value, Is.EqualTo(123));
+        }
+
+        private static Generic<T, U> CreateGeneric<T, U>(Container container, Type _)
+        {
+            return new Generic<T, U>();
+        }
+
+        private static void InvalidReturnCreator<T, U>(Container container, Type _)
+        {
+        }
+
+        private static Generic<T, U> InvalidParameterCreator<T, U>(Container container)
+        {
+            return new Generic<T, U>();
+        }
+
+        [Test]
+        public void should_register_and_resolve_generic_with_multiple_type_parameters_by_method_name()
+        {
+            var container = new Container();
+            var creator = GetType().GetMethod(nameof(CreateGeneric), BindingFlags.Static | BindingFlags.NonPublic);
+            container.RegisterGeneric(typeof(Generic<,>), creator).AsSelf();
+            var instance = container.Resolve<Generic<int, float>>();
+        }
+
+        [Test]
+        public void should_throw_exception_if_creator_is_not_valid()
+        {
+            var container = new Container();
+            Assert.Throws<ArgumentNullException>(() => container.RegisterGeneric(typeof(Generic<,>), null).AsSelf());
+
+            var lazyCreator = GetType().GetMethod(nameof(CreateLazy), BindingFlags.Static | BindingFlags.NonPublic);
+            Assert.Throws<ArgumentException>(() => container.RegisterGeneric(typeof(Generic<>), lazyCreator).AsSelf());
+            Assert.Throws<ArgumentException>(() => container.RegisterGeneric(typeof(Generic<,>), lazyCreator).AsSelf());
+
+            var genericCreator = GetType().GetMethod(nameof(CreateGeneric), BindingFlags.Static | BindingFlags.NonPublic);
+            Assert.Throws<ArgumentException>(() => container.RegisterGeneric(typeof(Generic<>), genericCreator).AsSelf());
+
+            var invalidReturnCreator = GetType().GetMethod(nameof(InvalidReturnCreator), BindingFlags.Static | BindingFlags.NonPublic);
+            Assert.Throws<ArgumentException>(() => container.RegisterGeneric(typeof(Generic<,>), invalidReturnCreator).AsSelf());
+
+            var invalidParameterCreator = GetType().GetMethod(nameof(InvalidParameterCreator), BindingFlags.Static | BindingFlags.NonPublic);
+            Assert.Throws<ArgumentException>(() => container.RegisterGeneric(typeof(Generic<,>), invalidParameterCreator).AsSelf());
         }
     }
 }
